02444ab1388b4b8e63bf9ea48f9dcb3cacceca43
Ned Schwartz <ned@theinterned.net>
2017-09-01 15:10:36 -0400

    recreates the main OBT layout with glamorous-primitives

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import glam from 'glamorous-primitives';
+
+const { Text } = glam;
+
+const Screen = glam.view({
+  alignItems: 'center',
+  backgroundColor: '#50e2c1',
+  padding: 10,
+});
+
+const Header = glam.view({
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  padding: 10,
+  marginBottom: 10,
+  borderBottomWidth: 1,
+  borderBottomColor: 'rgba(0,0,0,0.25)',
+});
+
+const Body =  glam.view({
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  padding: 10,
+  backgroundColor: '#fff',
+  borderRadius: 3,
+  padding: 10,
+});
+
+const Title = glam.text({
+  color: '#fff',
+  fontWeight: 'bold',
+});
 
 const App = () =>
-  <div className="App">
-    <div className="App-header">
-      <h1>One Big Thing</h1>
-    </div>
-    <p className="App-intro">Do it</p>
-  </div>;
+  <Screen>
+    <Header>
+      <Title>One Big Thing</Title>
+    </Header>
+    <Body>
+      <Text>Do it</Text>
+    </Body>
+  </Screen>;
 
 export default App;
