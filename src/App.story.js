ec34fab01c51434132f69e296ac8026360a7341e
Ned Schwartz <ned@theinterned.net>
2017-09-01 18:58:13 -0400

    cleans up mocks in App story

--- src/App.story.js
+++ src/App.story.js
@@ -2,32 +2,36 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import App from './App';
 
+const bigThing = { todo: 'Create slide deck for the big meeting' }
+const thingTwo = { todo: 'Sign the contracts' }
+const thingThree = { todo: 'Email vendors' }
+
 storiesOf('App', module)
   .add('empty', () => (
     <App />
   ))
   .add('with one big thing to do', () => (
     <App todos={[
-      { todo: 'My main thing', done: false }
+      bigThing,
     ]} />
   ))
   .add('with one big thing done', () => (
     <App todos={[
-      { todo: 'My main thing', done: true }
+      { ...bigThing, done: true }
     ]} />
   ))
   .add('with three things to do', () => (
     <App todos={[
-      { todo: 'My main thing', done: false },
-      { todo: 'Sign the contracts', done: false },
-      { todo: 'Email vendors', done: false },
+      bigThing,
+      thingTwo,
+      thingThree,
     ]} />
   ))
   .add('with a mix of done and not done', () => (
     <App todos={[
-      { todo: 'My main thing', done: true },
-      { todo: 'Sign the contracts', done: false },
+      { ...bigThing, done: true },
+      thingTwo,
       undefined,
-      { todo: 'Email vendors', done: true },
+      { ...thingThree, done: true },
     ]} />
   ));
