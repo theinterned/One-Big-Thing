a16c5550f6eedd3378334ff6e1eea8dccd902a4c
Ned Schwartz <ned@theinterned.net>
2017-09-01 13:38:23 -0400

    updates the storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
+import React from 'react';
 import { configure } from '@storybook/react';
 
+const req = require.context(
+  '../src', // path where stories live
+  true, // recursive?
+  /\.story.js$/ // story files match this pattern
+);
+
 function loadStories() {
-  require('../stories');
+  req.keys().forEach(module => req(module));
 }
 
 configure(loadStories, module);
