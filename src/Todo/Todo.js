6074bfb06511333167a4baad44bfb112b89cee1a
Ned Schwartz <ned@theinterned.net>
2017-09-01 16:38:33 -0400

    styles Todo empty state

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,13 +1,32 @@
 import React from "react";
 import glam from 'glamorous-primitives';
 
-const { View, Text } = glam;
+const { View } = glam;
+
+const Input = glam.text({
+  fontSize: 120,
+  fontWeight: 'lighter',
+  color: '#ccc',
+});
+
+const Button = glam.text({
+  color: '#ccc',
+  textTransform: 'uppercase',
+});
 
 export default function Todo() {
   return (
-    <View>
-      <Text>+</Text>
-      <Text>Big Thing</Text>
+    <View
+      height='500px'
+      alignItems="center"
+      justifyContent="center"
+      padding={10}
+    >
+      <View
+        flex={1}
+        justifyContent="center"
+      ><Input>+</Input></View>
+      <Button>Big Thing</Button>
     </View>
   );
 }
